3d3f67ffbc7997836309fec27878a17f4c055d06
Eone-tgx <eone1024@gmail.com>
2025-11-21 22:14:35 +0200

    setup-template

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'modern-normalize';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
